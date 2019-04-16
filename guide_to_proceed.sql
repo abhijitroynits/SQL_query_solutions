5530fbd1ac9d38bbf386cb231e5c6721212fc66c
Abhijit Roy <abhijitroy.nits@gmail.com>
2019-04-16 11:02:38 -0500

    Add files via upload

---
+++ guide_to_proceed.sql
@@ -0,0 +1,5 @@
+/* Since for each quiz, there's a preceding story or a description,
+ * for the sake of brevity, the SQL queries that have been solved 
+ * are included in the .doc files.
+ * Kindly refer to the attached docs for a systematic practice / evaluation.
+ */ 
