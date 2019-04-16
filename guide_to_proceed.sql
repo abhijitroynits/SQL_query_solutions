5e0ee9cff9f242a17082dfb042025140d896d822
Abhijit Roy <abhijitroy.nits@gmail.com>
2019-04-16 11:09:38 -0500

    Update guide_to_proceed.sql

--- guide_to_proceed.sql
+++ guide_to_proceed.sql
@@ -1,5 +1,5 @@
 /* Since for each quiz, there's a preceding story or a description,
  * for the sake of brevity, the SQL queries that have been solved 
- * are included in the .doc files.
+ * are included in the .doc files only.
  * Kindly refer to the attached docs for a systematic practice / evaluation.
  */ 
